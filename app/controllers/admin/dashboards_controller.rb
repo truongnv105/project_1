d94ce4da5aed61632b8be5f619fb4427f7fbc35e
TruongNV <truongnv105>
2018-07-19 16:01:47 +0700

    Finish refactor admin layout

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardsController < Admin::ApplicationController
   def index
+    unless !current_user.nil? && current_user.is_admin?
+      redirect_to root_path
+    end
   end
 end
