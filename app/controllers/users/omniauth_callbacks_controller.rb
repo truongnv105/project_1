6f769ef5b2c3a9688d17ac39257751f5efbed5c9
TruongNV <truongnv105>
2018-07-22 11:24:00 +0700

    Finish refactor code

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,37 +1,36 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    @user = User.from_omniauth_facebook(request.env["omniauth.auth"])
-
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
-    else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
+    generic_callback("Facebook")
   end
 
   def google_oauth2
-    @user = User.from_omniauth_google(request.env["omniauth.auth"])
-
-    if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Google"
-      sign_in_and_redirect @user, event: :authentication
-    else
-      session["devise.google_data"] = request.env["omniauth.auth"].except(:extra)
-      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
-    end
+    generic_callback("Google_oauth2")
   end
 
   def twitter
-    @user = User.from_omniauth_twitter(request.env["omniauth.auth"])
+    generic_callback("Twitter")
+  end
 
-    if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: "Twitter"
-      sign_in_and_redirect @user, event: :authentication
+  def generic_callback provider
+    if !current_user.nil?
+      if !current_user.providers.where(provider: request.env["omniauth.auth"]["provider"],
+          uid: request.env["omniauth.auth"]["uid"]).present?
+        current_user.providers.create(provider: request.env["omniauth.auth"]["provider"],
+        uid: request.env["omniauth.auth"]["uid"], email: request.env["omniauth.auth"]["info"]["email"],
+        token: request.env["omniauth.auth"]["credentials"]["token"])
+        redirect_to edit_user_registration_url(current_user.id), alert: t(".add_success") + provider
+      else
+        redirect_to edit_user_registration_url(current_user.id), alert: t(".linking_was_exist")
+      end
     else
-      session["devise.twitter_data"] = request.env["omniauth.auth"].except(:extra)
-      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+
+      if !@user.nil?
+        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", kind: provider
+        sign_in_and_redirect @user, event: :authentication
+      else
+        redirect_to new_user_registration_url, alert: t(".account") + provider + t(".notice")
+      end
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,36 +1,44 @@
 class User < ApplicationRecord
+  attr_accessor :login
+
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable,
     :omniauthable, :omniauth_providers => [:facebook, :google_oauth2, :twitter]
+
   mount_uploader :avatar, PictureUploader
+  validates :phone_number, length: {maximum: 11, minimum: 10}, if: :email_present?
+  has_many :providers, dependent: :destroy
 
-  def self.from_omniauth_facebook(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name
-      user.image = auth.info.image
+  def login
+    @login || self.phone_number || self.email
+  end
+
+  class << self
+    def find_for_database_authentication warden_conditions
+      conditions = warden_conditions.dup
+      login = conditions.delete(:login)
+      where(conditions).where(["lower(phone_number) = :value OR lower(email) = :value",
+        {value: login.strip.downcase}]).first
+    end
+
+    def from_omniauth auth
+      user_id = Provider.where(provider: auth["provider"], uid: auth["uid"]).pluck(:user_id).first
+      user = User.find_by(id: user_id)
+      user
     end
   end
 
-  def self.from_omniauth_google(access_token)
-    data = access_token.info
-    user = User.where(email: data['email']).first
+  private
 
-    # Uncomment the section below if you want users to be created if they don't exist
-    unless user
-      user = User.create(name: data['name'],
-        email: data['email'],
-        password: Devise.friendly_token[0,20]
-      )
-    end
-    user
+  def email_required?
+    !phone_number.present?
   end
 
- def self.from_omniauth_twitter(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-    end
+  def email_changed?
+    !phone_number.present?
+  end
+
+  def email_present?
+    !email.present?
   end
 end
